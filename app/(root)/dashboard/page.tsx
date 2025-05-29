22f9c735f8ff5347fcd6e6ec7cf41b29a789a576
ookapratama <ookapratama2@gmail.com>
2025-05-29 23:56:02 +0800

    feat : add skeleton dashboard

--- app/(root)/dashboard/page.tsx
+++ app/(root)/dashboard/page.tsx
@@ -3,6 +3,7 @@ import PageContainer from "@/components/common/page-container";
 import PageHeader from "@/components/common/page-header";
 import SectionHeading from "@/components/common/section-header";
 import SectionSubHeading from "@/components/common/section-sub-header";
+import { SkeletonCalendar, SkeletonCard } from "@/components/dashboard/skeleton/skeleton-card";
 import { pagesConfig } from "@/config/pages";
 import { SocialLinks } from "@/config/socials";
 import { Metadata } from "next";
@@ -25,7 +26,7 @@ const DashboardPage = () => {
           My contributions from last year on github.
         </p>
         <Link
-          href={SocialLinks[0].username}
+          href={SocialLinks[0].link}
           target="_blank"
           passHref
           className="text-sm font-code text-neutral-400 dark:text-neutral-600 hover:text-neutral-700 hover:dark:text-neutral-400"
@@ -33,6 +34,13 @@ const DashboardPage = () => {
           {SocialLinks[0].username}
         </Link>
       </SectionSubHeading>
+      <div className="grid grid-cols-2 gap-3 py-2 sm:grid-cols-4">
+        <SkeletonCard />
+        <SkeletonCard />
+        <SkeletonCard />
+        <SkeletonCard />
+      </div>
+      <SkeletonCalendar className="mt-5" />
     </PageContainer>
   );
 };
---
+++ components/dashboard/skeleton/skeleton-card.tsx
@@ -0,0 +1,34 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import { Card } from "../../ui/card";
+
+interface SkeletonProps {
+  className?: string;
+}
+
+export const SkeletonCard = ({ className = "" }: SkeletonProps) => {
+  return (
+    <Skeleton className={`rounded-xl  ${className}`}>
+      <Card className="h-[100px] content-center">
+        <div className="space-y-5 px-3">
+          <Skeleton className="h-4 max-w-[150px]" />
+          <Skeleton className="h-4 max-w-[100px]" />
+        </div>
+      </Card>
+    </Skeleton>
+  );
+};
+
+export const SkeletonCalendar = ({ className = "" }: SkeletonProps) => {
+  return (
+    <>
+      <Skeleton className={`rounded-xl ${className}`}>
+        <div className="space-y-28 my-5 px-3">
+          <Skeleton className="h-4 max-w-[150px]" />
+
+          <Skeleton className="h-4 max-w-[100px]" />
+        </div>
+      </Skeleton>
+      <Skeleton className="h-4 max-w-[150px]" />
+    </>
+  );
+};
