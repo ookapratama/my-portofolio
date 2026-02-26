78fa13b44e778e5c30a5c504ed2ec41c68364a3a
ookapratama <ookapratama2@gmail.com>
2026-02-27 00:17:32 +0800

    feat: Localize GitHub redirect card text to Indonesian, update its repository link, and remove a commented-out GitHub link from the layout.

--- components/contact/github-redirect-card.tsx
+++ components/contact/github-redirect-card.tsx
@@ -25,20 +25,20 @@ export default function GithubRedirectCard() {
           />
         </div>
         <h2 className="font-heading text-xl tracking-tight lg:text-3xl duration-300">
-          Like this template?
+          Tertarik dengan kodenya?
         </h2>
         <p className="mt-2 mb-10 font-heading text-lg text-muted-foreground">
-          It&#39;s open source. Explore and contribute on GitHub.
+          Proyek ini bersifat open source. Jelajahi kodenya di GitHub.
         </p>
         <Github className="w-10 h-10 text-muted-foreground mb-5" />
       </CardContent>
       <CardFooter className="px-8 pb-8 pt-0">
         <Link
-          href={"https://github.com/namanbarkiya/minimal-next-portfolio"}
+          href={"https://github.com/ookapratama/my-portofolio"}
           target="_blank"
           className={cn(
             buttonVariants({ variant: "outline" }),
-            "w-full bg-transparent border-2 transition-all duration-300 py-6"
+            "w-full bg-transparent border-2 transition-all duration-300 py-6",
           )}
         >
           <span className="mr-2">Source Code</span>
